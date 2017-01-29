e878f99465eee526ce2e78d1bef860c6fffca777
Taro Minowa <higepon@gmail.com>
2017-01-29 09:32:31 +0900

    cnn

--- cnn.py
+++ cnn.py
@@ -18,11 +18,14 @@ def max_pool_2x2(x):
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+# 28x28=784 * n
 x = tf.placeholder(tf.float32, [None, 784])
+
+# 10 * n
 y_ = tf.placeholder(tf.float32, [None, 10])
 
 # why output size is 32?
-W_conv1 = weight_variable([5, 5, 1, 32])
+W_conv1 = weight_variable([5, 5, 1, 90])
 b_conv1 = bias_variable([32])
 
 x_image = tf.reshape(x, [-1, 28, 28, 1])
@@ -30,7 +33,7 @@ x_image = tf.reshape(x, [-1, 28, 28, 1])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
-W_conv2 = weight_variable([5, 5, 32, 64])
+W_conv2 = weight_variable([5, 5, 90, 64])
 b_conv2 = bias_variable([64])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
